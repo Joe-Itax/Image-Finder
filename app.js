202d2bc161f774332b4eca177fdce5b4ef373d94
Joseph ITAKALA <josephitakala18@gmail.com>
2023-10-09 16:56:12 +0100

    Add: the nextpage button

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const handleImageClicks = () => {
       document.body.classList.add('overflow-hidden');
       openOverlay.innerHTML = `
         <figure class="relative">
-          <img src="${image.src}" alt="${image.alt}" title="${image.title}" class="w-full"/>
+          <img src="${image.src}" alt="${image.alt}" title="${image.title}" class="w-full cursor-zoom-in"/>
           <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
             <div class="link-photographer">
               <a href="$" target="_blank" title="photographer: $" class="text-2xl md:text-4xl xl:text-7xl">
@@ -42,9 +42,6 @@ const handleBtnDownloadClick = () => {
       imageCorrespondant.forEach((image) => {
         // console.table(image.id);
         if (image.id === btn.id) {
-          console.log(`Btn-id-trueImage: ${btn.id}`);
-          console.log(`Btn-src-trueImage: ${image.src}`);
-          console.log(`Btn-Alt-trueImage: ${image.alt}`);
           // Récupérer le contenu de l'image
           fetch(image.src)
             .then((response) => response.blob())
@@ -182,7 +179,68 @@ const listDefaultQuery2 = [
   'stars',
   'innovation',
 ];
-
+// Next Page Btn function
+let page = 1;
+let nextPage = document.querySelector('.container-next-page');
+const btnNextPage = document.querySelector('#btn-nextPage');
+const nextPageDirection = (data) => {
+  let total_pages = data.total_pages;
+  if (total_pages || total_pages > 1) {
+    nextPage.classList.remove('hidden');
+    btnNextPage.addEventListener('click', () => {
+      page += 1;
+      url = url.replace(`&page=${page - 1}`, `&page=${page}`);
+      fetch(url)
+        .then((response) => response.json())
+        .then((newData) => {
+          ul1.innerHTML = '';
+          ul2.innerHTML = '';
+          ul3.innerHTML = '';
+          for (let i = 0; i < newData.results.length; i++) {
+            const li = document.createElement('li');
+            li.classList.add('mb-4');
+            li.innerHTML = `
+              <div class="image relative">
+                <figure class="relative w-full">
+                  <img src="${newData.results[i].urls.small}" alt="${newData.results[i].alt_description}" id="${newData.results[i].id}" title="${newData.results[i].alt_description}" class="classImageTest w-full cursor-zoom-in"/>
+                  <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
+                    <div class="link-photographer">
+                      <a href="${newData.results[i].user.links.html}" target="_blank" title="photographer: ${newData.results[i].user.name}" class="text-lg md:text-xl">
+                        <i class="ri-user-fill"></i>
+                      </a>
+                    </div>
+                    <div class="btn-download-box">
+                      <button type="button" title="Télécharger cette image" class="btnDownload" id="${newData.results[i].id}"><i class="ri-download-fill text-lg md:text-xl"></i></button>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
+            `;
+            if (i < 10) {
+              ul1.classList.add('col-start-1', 'col-end-2', 'max-h-full');
+              ul1.appendChild(li);
+            } else if (i < 20) {
+              ul2.classList.add('col-start-2', 'col-end-3', 'max-h-full');
+              ul2.appendChild(li);
+            } else {
+              ul3.classList.add(
+                'col-start-1',
+                'col-end-3',
+                'max-h-full',
+                'md:col-start-3',
+                'md:col-end-4'
+              );
+              ul3.appendChild(li);
+            }
+          }
+          handleImageClicks();
+          handleBtnDownloadClick();
+        });
+    });
+  } else {
+    nextPage.classList.add('hidden');
+  }
+};
 // Créer 3 éléments de liste ul
 let ul1 = document.createElement('ul');
 let ul2 = document.createElement('ul');
@@ -197,12 +255,11 @@ const query1 = listDefaultQuery1[randomQuery1];
 const query2 = listDefaultQuery2[randomQuery2];
 titltOfRsearch.textContent = `${query1} and ${query2}`;
 let query = titltOfRsearch.textContent;
-let url = `https://api.unsplash.com/search/photos/?query=${query}&per_page=30&client_id=OHXabCxP14DXrZMvtHr7qFjUeLe7DvnncGA_LAC_dm0`;
+let url = `https://api.unsplash.com/search/photos/?query=${query}&per_page=30&page=${page}&client_id=OHXabCxP14DXrZMvtHr7qFjUeLe7DvnncGA_LAC_dm0`;
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    console.log(data.results);
+    nextPageDirection(data);
 
     for (let i = 0; i < data.results.length; i++) {
       const li = document.createElement('li');
@@ -210,7 +267,7 @@ fetch(url)
       li.innerHTML = `
       <div class="image relative">
         <figure class="relative w-full">
-          <img src="${data.results[i].urls.small}" alt="${data.results[i].alt_description}" id="${data.results[i].id}" title="${data.results[i].alt_description}" class="classImageTest w-full"/>
+          <img src="${data.results[i].urls.small}" alt="${data.results[i].alt_description}" id="${data.results[i].id}" title="${data.results[i].alt_description}" class="classImageTest w-full cursor-zoom-in"/>
           <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
             <div class="link-photographer">
               <a href="${data.results[i].user.links.html}" target="_blank" title="photographer: ${data.results[i].user.name}" class="text-lg md:text-xl">
@@ -254,13 +311,12 @@ const onValidate = (event) => {
   query = searchValue.value;
   titltOfRsearch.classList.remove('hidden');
   titltOfRsearch.textContent = query;
-  console.log(query);
-  url = `https://api.unsplash.com/search/photos/?page=1&query=${query}&per_page=30&client_id=OHXabCxP14DXrZMvtHr7qFjUeLe7DvnncGA_LAC_dm0`;
+  url = `https://api.unsplash.com/search/photos/?page=1&query=${query}&per_page=30&${page}&client_id=OHXabCxP14DXrZMvtHr7qFjUeLe7DvnncGA_LAC_dm0`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      nextPageDirection(data);
       console.log(data);
-      // console.log(data.photos[0].id);
       let html = ''; //Variable qui va stocker le code html
       for (let i = 0; i < data.results.length; i++) {
         const li = document.createElement('li');
@@ -268,7 +324,7 @@ const onValidate = (event) => {
         li.innerHTML = `
           <div class="image relative">
             <figure class="relative">
-              <img src="${data.results[i].urls.small}" alt="${data.results[i].alt_description}" id="${data.results[i].id}" title="${data.results[i].alt_description}" class="classImageTest"/>
+              <img src="${data.results[i].urls.small}" alt="${data.results[i].alt_description}" id="${data.results[i].id}" title="${data.results[i].alt_description}" class="classImageTest cursor-zoom-in"/>
               <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
                 <div class="link-photographer">
                   <a href="${data.results[i].user.links.html}" target="_blank" title="photographer: ${data.results[i].user.name}" class="text-lg md:text-xl">
@@ -303,124 +359,6 @@ const onValidate = (event) => {
       handleBtnDownloadClick();
     });
 };
-/*
-let url = `https://api.pexels.com/v1/search?query=${query1}+and+${query2}&per_page=18`;
-fetch(url, {
-  headers: {
-    Authorization: 'PUpg6u4IM4rBsAkLq5vVcmS4c03dj6TkcVOrDM9AYPo6tE9UUqhw77UC',
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    // console.log(data.photos[0].id);
-
-    // Parcour de la boucle for qui génère le code HTML des images
-    // Ajout des éléments li à la liste ul appropriée
-    for (let i = 0; i < data.photos.length; i++) {
-      const li = document.createElement('li');
-      li.classList.add('mb-4');
-      li.innerHTML = `
-      <div class="image relative">
-        <figure class="relative">
-          <img src="${data.photos[i].src.large2x}" alt="${data.photos[i].alt}" id="${data.photos[i].id}" title="${data.photos[i].alt}" class="classImageTest"/>
-          <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
-            <div class="link-photographer">
-              <a href="${data.photos[i].photographer_url}" target="_blank" title="photographer: ${data.photos[i].photographer}" class="text-lg md:text-xl">
-                <i class="ri-user-fill"></i>
-              </a>
-            </div>
-            <div class="btn-download-box">
-              <button type="button" title="Télécharger cette image" class="btnDownload" id="${data.photos[i].id}"><i class="ri-download-fill text-lg md:text-xl"></i></button>
-            </div>
-          </figcaption>
-        </figure>
-      </div>
-    `;
-      if (i < 6) {
-        ul1.classList.add('col-start-1', 'col-end-2', 'max-h-full');
-        ul1.appendChild(li);
-      } else if (i < 12) {
-        ul2.classList.add('col-start-2', 'col-end-3', 'max-h-full');
-        ul2.appendChild(li);
-      } else {
-        ul3.classList.add(
-          'col-start-1',
-          'col-end-3',
-          'max-h-full',
-          'md:col-start-3',
-          'md:col-end-4'
-        );
-        ul3.appendChild(li);
-      }
-    }
-    handleImageClicks();
-    handleBtnDownloadClick();
-  });
-
-//Selon l'input du user
-const onValidate = (event) => {
-  event.preventDefault();
-  ul1.innerHTML = '';
-  ul2.innerHTML = '';
-  ul3.innerHTML = '';
-  const query = searchValue.value;
-  titltOfRsearch.classList.remove('hidden');
-  titltOfRsearch.textContent = query;
-  console.log(query);
-  url = `https://api.pexels.com/v1/search?query=${query}&per_page=180`;
-  fetch(url, {
-    headers: {
-      Authorization: 'PUpg6u4IM4rBsAkLq5vVcmS4c03dj6TkcVOrDM9AYPo6tE9UUqhw77UC',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // console.log(data.photos[0].id);
-      let html = ''; //Variable qui va stocker le code html
-      for (let i = 0; i < data.photos.length; i++) {
-        const li = document.createElement('li');
-        li.classList.add('mb-4');
-        li.innerHTML = `
-          <div class="image relative">
-            <figure class="relative">
-              <img src="${data.photos[i].src.large2x}" alt="${data.photos[i].alt}" id="${data.photos[i].id}" title="${data.photos[i].alt}" class="classImageTest"/>
-              <figcaption class="detail-img h-auto bg-transparent backdrop-blur-sm absolute w-full left-0 bottom-0 p-1 text-white flex justify-between md:text-xl px-2.5">
-                <div class="link-photographer">
-                  <a href="${data.photos[i].photographer_url}" target="_blank" title="photographer: ${data.photos[i].photographer}" class="text-lg md:text-xl">
-                    <i class="ri-user-fill"></i>
-                  </a>
-                </div>
-                <div class="btn-download-box">
-                  <button title="Télécharger cette image" class="btnDownload" id="${data.photos[i].id}"><i class="ri-download-fill text-lg md:text-xl"></i></button>
-                </div>
-              </figcaption>
-            </figure>
-          </div>
-        `;
-        if (i < 6) {
-          ul1.classList.add('col-start-1', 'col-end-2', 'max-h-full');
-          ul1.appendChild(li);
-        } else if (i < 12) {
-          ul2.classList.add('col-start-2', 'col-end-3', 'max-h-full');
-          ul2.appendChild(li);
-        } else {
-          ul3.classList.add(
-            'col-start-1',
-            'col-end-3',
-            'max-h-full',
-            'md:col-start-3',
-            'md:col-end-4'
-          );
-          ul3.appendChild(li);
-        }
-      }
-      handleImageClicks();
-      handleBtnDownloadClick();
-    });
-};
-*/
 
 btnValidate.addEventListener('click', onValidate);
 
@@ -431,45 +369,3 @@ searchValue.addEventListener('keydown', (event) => {
     onValidate(event); // Appelle la fonction onValidate avec l'événement en paramètre
   }
 });
-
-console.log(titltOfRsearch.textContent);
-
-// Test de la Fonction de téléchargement
-/*const downloadImage = async (event) => {
-
-  const image = event.target.previousElementSibling;
-  const imageUrl = image.src;
-  console.log(`1- image=${image}. imageUrl=${imageUrl} `);
-  const url = new URL(imageUrl);
-  url.download = 'image.jpg';
-  console.log(`2- url=${url}. url.download=${url.download} `);
-  try {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.click();
-  } catch (err) {
-    console.error('Error downloading image', err);
-  }
-}
-const downloadImage = async (event) => {
-  const image = event.target.previousElementSibling;
-  const imageUrl = image.src;
-
-  try {
-    const response = await fetch(imageUrl);
-    const blob = await response.blob();
-
-    const download = await navigator.downloads.download({
-      url: URL.createObjectURL(blob),
-      filename: 'image.jpg',
-      saveAs: true
-    });
-
-    URL.revokeObjectURL(download.url);
-
-  } catch (error) {
-    console.error('Error downloading image:', error);
-  }
-}*/
